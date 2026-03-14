49989609dfa3d34d04e4c2673ab75904351e47eb
gotayjosue <gotayjosue@gmail.com>
2026-03-14 10:11:50 -0600

    Starting Journey

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+//This is a simple console application that prints "Hello World" to the console.
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello World");
+    }
+}
